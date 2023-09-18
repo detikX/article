2f7ba7f0efa3b49fd6b7ddba7c560a4a5659050c
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-09-18 11:20:56 +0700

    frfrf

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    AOS.init()
+})
+
+$(".data-data p").attr({ "data-aos": "zoom-in", "data-aos-delay": "1000", "data-aos-duration": "500" })
+
+$(".data-data .thumb").attr({ "data-aos": "fade-in", "data-aos-delay": "1000", "data-aos-duration": "500" })

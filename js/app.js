3ae4f2bb66d369cd60fdd1c9887e20aa7b7a063d
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-09-19 11:25:19 +0700

    alkaka

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     AOS.init()
 })
 
-$(".data-data p, .data-data .line-right, .data-data .data").attr({ "data-aos": "fade-up", "data-aos-delay": "500", "data-aos-duration": "500" })
+$(".data-data p, .data-data .line-right, .data-data .data,.data-data .data-all, .data-data .bacajuga").attr({ "data-aos": "fade-up", "data-aos-delay": "500", "data-aos-duration": "500" })
 
 $(".data-data .thumb").attr({ "data-aos": "fade-in", "data-aos-delay": "500", "data-aos-duration": "500" })

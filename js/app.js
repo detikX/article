02aa56329c1c46fd21688618cd44a64e1f617368
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-09-25 09:19:47 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     AOS.init()
 })
 
-$(".data-data p, .data-data .line-right, .data-data .data,.data-data .data-all, .data-data .bacajuga").attr({ "data-aos": "fade-up", "data-aos-delay": "300", "data-aos-duration": "300" })
+$(".data-data p, .data-data .line-right, .data-data .data,.data-data .data-all, .data-data .bacajuga").attr({ "data-aos": "fade-up", "data-aos-delay": "300", "data-aos-duration": "500" })
 
-$(".data-data .thumb").attr({ "data-aos": "fade-in", "data-aos-delay": "300", "data-aos-duration": "300" })
+$(".data-data .thumb").attr({ "data-aos": "fade-in", "data-aos-delay": "300", "data-aos-duration": "500" })

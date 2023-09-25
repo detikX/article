a9f971bfb05d389e83a4156b0d08ed9cad7402a9
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-09-25 08:20:55 +0700

    s

---
+++ detikx-aos/app-dua.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    AOS.init()
+})
+
+$(".data-data p, .data-data .line-right, .data-data .data,.data-data .data-all").attr({ "data-aos": "fade-up", "data-aos-delay": "500", "data-aos-duration": "500" })

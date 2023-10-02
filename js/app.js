808868106ecb71eb5dda9de91db664036d0eb6a8
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-10-02 11:13:42 +0700

    f

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
     AOS.init()
 })
+$(window).on("resize", function() {
+    AOS.refresh()
+})
 
 $(".data-data p, .data-data .line-right, .data-data .data,.data-data .data-all, .data-data .bacajuga").attr({ "data-aos": "fade-up", "data-aos-delay": "300", "data-aos-duration": "300","data-aos-offset": "20", "data-aos-easing": "linear" })
 
-$(".data-data .thumb").attr({ "data-aos": "fade-in", "data-aos-delay": "300", "data-aos-duration": "500" })
+$(".data-data .thumb").attr({ "data-aos": "fade-in", "data-aos-delay": "300", "data-aos-duration": "500", "data-aos-duration": "300","data-aos-offset": "20", "data-aos-easing": "linear" })

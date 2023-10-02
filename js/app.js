2e64974539c3338bd1bc0f7541a11b1a076dcacf
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-10-02 10:34:32 +0700

    frf

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     AOS.init()
 })
 
-$(".data-data p, .data-data .line-right, .data-data .data,.data-data .data-all, .data-data .bacajuga").attr({ "data-aos": "fade-up", "data-aos-delay": "300", "data-aos-duration": "300","data-aos-offset": "60", "data-aos-easing": "linear" })
+$(".data-data p, .data-data .line-right, .data-data .data,.data-data .data-all, .data-data .bacajuga").attr({ "data-aos": "fade-up", "data-aos-delay": "300", "data-aos-duration": "300","data-aos-offset": "20", "data-aos-easing": "linear" })
 
 $(".data-data .thumb").attr({ "data-aos": "fade-in", "data-aos-delay": "300", "data-aos-duration": "500" })
